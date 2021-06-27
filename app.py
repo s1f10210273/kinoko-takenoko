724dbd37a1b40197091c2f8a40a43cdea1ca3b94
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2021-06-28 00:34:50 +0900

    課題

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import re
 app = Flask(__name__)
+
+kinoko_count = 3
+takenoko_count = 5
+messages=["Kinoko is wonrderful!","Takenoko is owesome!"]
 
 @app.route('/')
 def top():
@@ -7,6 +12,28 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count, takenoko_count, messages
+    if request.form.get("item") == "kinoko":
+        kinoko_count += 1
+    elif request.form.get("item") == "takenoko":
+        takenoko_count += 1
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+
+    messages.append(request.form.get("message"))
+    if len(messages) > 3:
+        messages = messages[-3:]
+
+    message_html= ""
+    for i in range(len(messages)):
+        message = messages[i]
+        message = re.sub(r"&" , r"&amp" , message)
+        message = re.sub(r"<" , r"&lt" , message)
+        message = re.sub(r"<" , r"&gt" , message)
+        message = re.sub(r"\*(.+)\*" , r"<strong>\1</strong>" , message)
+        message = re.sub(r'\d{2,3}-\d+-\d+' , r'\1-＊＊＊＊-＊＊＊＊' , message)
+        #message = re.sub(r"https://+" , r"<a href='\1'>\1</a>" , message)
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n' .format(message, "alert-warning ms-5" if i % 2 == 0 else "alert-success me-5")
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
